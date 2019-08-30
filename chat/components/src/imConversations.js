f03e6baa7db9d9fd9d824be8fe8f768d5a1e17dd
Paul Morris <paul@thunderbird.net>
2019-08-30 14:45:07 -0400

    Bug 1578477 - Fix 'if, else, comments', other formatting in chat/. r=mkmelin
    Ported from bug 1577835.

--- chat/components/src/imConversations.js
+++ chat/components/src/imConversations.js
@@ -297,8 +297,9 @@ UIConversation.prototype = {
   },
   checkClose() {
     if (!this._currentTargetId) {
+      // Already closed.
       return true;
-    } // already closed.
+    }
 
     if (
       !Services.prefs.getBoolPref("messenger.conversations.alwaysClose") &&
@@ -411,8 +412,9 @@ UIConversation.prototype = {
 
     this._disconnected = true;
     if (this.contact) {
+      // Handled by the contact observer.
       return;
-    } // handled by the contact observer.
+    }
 
     if (this.isChat && this.left) {
       this._wasLeft = true;
@@ -573,8 +575,9 @@ UIConversation.prototype = {
 
     for (let observer of this._observers) {
       if (!observer.observe && !this._observers.includes(observer)) {
+        // Observer removed by a previous call to another observer.
         continue;
-      } // observer removed by a previous call to another observer.
+      }
       observer.observe(aSubject, aTopic, aData);
     }
     this._notifyUnreadCountChanged();
