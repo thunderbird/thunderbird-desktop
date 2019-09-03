c5bc188da149beefcd1e58d58608549e45b800a8
Ian Neal <iann_cvs@blueyonder.co.uk>
2019-09-03 21:30:29 +0200

    Bug 1575201 - Port Bug 1040947 "Opening page info from a remote tab is sluggish" to SeaMonkey. r=frg a=frg

--- suite/base/content/nsContextMenu.js
+++ suite/base/content/nsContextMenu.js
@@ -970,7 +970,8 @@ nsContextMenu.prototype = {
   },
 
   viewFrameInfo: function() {
-    BrowserPageInfo(this.target.ownerDocument);
+    BrowserPageInfo(this.target.ownerDocument, null, null,
+                    gContextMenuContentData.frameOuterWindowID);
   },
 
   toggleImageSize: function() {
