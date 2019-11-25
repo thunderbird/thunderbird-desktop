085d68585214450a598e59db07c57eeb43911fa8
Neil Rashbrook <neil@parkwaycc.co.uk>
2019-11-25 11:12:50 +0000

    Bug 1598041 - Don't hide error message when offering addon r=BenB a=mkmelin

--- mail/components/accountcreation/content/emailWizard.js
+++ mail/components/accountcreation/content/emailWizard.js
@@ -974,7 +974,6 @@ EmailConfigWizard.prototype = {
           _hide("result_addon_install");
           _enable("create_button");
         } else {
-          _hide("status_area");
           _show("result_addon_intro");
           var msg = gStringsBundle.getString("addon-intro");
           if (
