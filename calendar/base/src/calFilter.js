1d98e0dced67f8c69d82005bf30107700603ad31
Geoff Lankow <geoff@darktrojan.net>
2020-06-30 10:55:25 +1200

    Bug 1593711 - Remove extraneous QueryInterface calls that broke task filtering. r+a=pmorris

--- calendar/base/src/calFilter.js
+++ calendar/base/src/calFilter.js
@@ -798,7 +798,6 @@ calFilter.prototype = {
         if (this.isItemInFilters(next)) {
           return next;
         }
-        next.QueryInterface(Ci.calIEvent);
         start = next.startDate || next.entryDate;
       }
 
@@ -811,7 +810,6 @@ calFilter.prototype = {
       let exMatch = null;
       aItem.recurrenceInfo.getExceptionIds({}).forEach(function(rID) {
         let ex = aItem.recurrenceInfo.getExceptionFor(rID);
-        ex.QueryInterface(Ci.calIEvent);
         if (
           ex &&
           cal.dtz.now().compare(ex.startDate || ex.entryDate) < 0 &&
