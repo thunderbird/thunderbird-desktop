71a0645c82d2d939a22759e0b50e702f490e6729
Gene Smith <gds@chartertn.net>
2020-07-22 12:57:04 +0300

    Bug 1611624 - Ensure all imap capabilities are known after authentication (support IDLE for iCloud). r=mkmelin a=wsmwk
    At least one popular imap server (Apple's iCloud) fails to reveal its
    full capability set in the initial greeting and with authentication.
    This caused an iCloud user to notice that IMAP idle was not working so
    immediate notification of received messages never occurred in TB.
    This patch causes capabilties to be explicitly requested if they are
    not returned as part of the authentication response to ensure that
    IDLE and other important capabilities are know and used in TB.
    The additional capability request is added to the CLIENTID unit test since
    "fakeserver" behaves in this respect the same way as iCloud.

--- mailnews/imap/src/nsImapServerResponseParser.cpp
+++ mailnews/imap/src/nsImapServerResponseParser.cpp
@@ -2016,7 +2016,10 @@ void nsImapServerResponseParser::capability_data() {
   NS_ASSERTION(navCon,
                "null imap protocol connection while parsing capability "
                "response");  // we should always have this
-  if (navCon) navCon->CommitCapability();
+  if (navCon) {
+    navCon->CommitCapability();
+    fServerConnection.SetCapabilityResponseOccurred();
+  }
   skip_to_CRLF();
 }
 
