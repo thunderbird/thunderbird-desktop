238253a45add1cbf5cbac070434c3117b0ef853d
Geoff Lankow <geoff@darktrojan.net>
2020-08-03 13:43:43 +0300

    Bug 1652478 - Fix execution of content scripts in external popup windows. r=mkmelin a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D85372

--- mail/components/extensions/parent/ext-mail.js
+++ mail/components/extensions/parent/ext-mail.js
@@ -828,6 +828,9 @@ class Tab extends TabBase {
     if (this.isComposeTab) {
       return this.nativeTab.GetCurrentEditorElement();
     }
+    if (this.nativeTab.getBrowser) {
+      return this.nativeTab.getBrowser();
+    }
     return null;
   }
 
