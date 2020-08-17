37841bc3847e62da6e3b906b9403636f306fc146
aceman <acelists@atlas.sk>
2020-08-17 13:57:21 +0300

    Bug 1653690 - do not report failure if a filter tries to move a message to the same folder. r=mkmelin a=wsmwk

--- mailnews/local/src/nsParseMailbox.cpp
+++ mailnews/local/src/nsParseMailbox.cpp
@@ -1955,8 +1955,8 @@ NS_IMETHODIMP nsParseNewMailState::ApplyFilterHit(nsIMsgFilter* filter,
               }
             }
           } else {
-            MOZ_LOG(FILTERLOGMODULE, LogLevel::Warning,
-                    ("(Local) Target folder is the same as source folder"));
+            MOZ_LOG(FILTERLOGMODULE, LogLevel::Info,
+                    ("(Local) Target folder is the same as source folder, skipping"));
             rv = NS_OK;
           }
           *applyMore = false;
@@ -2000,8 +2000,8 @@ NS_IMETHODIMP nsParseNewMailState::ApplyFilterHit(nsIMsgFilter* filter,
             } else
               m_msgCopiedByFilter = true;
           } else {
-            MOZ_LOG(FILTERLOGMODULE, LogLevel::Warning,
-                    ("(Local) Target folder is the same as source folder"));
+            MOZ_LOG(FILTERLOGMODULE, LogLevel::Info,
+                    ("(Local) Target folder is the same as source folder, skipping"));
             break;
           }
         } break;
