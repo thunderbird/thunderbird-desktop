327bed958a8bd785f46ae76a8f7a707efc5d211e
Magnus Melin <mkmelin+mozilla@iki.fi>
2020-09-15 13:56:48 +0300

    Bug 1664858 - fix Crash in [@ MsgHdrsToTArray]. r=benc a=wsmwk

--- mailnews/base/src/nsMsgUtils.cpp
+++ mailnews/base/src/nsMsgUtils.cpp
@@ -1882,6 +1882,10 @@ void MsgRemoveQueryPart(nsCString& aSpec) {
 }
 
 void MsgHdrsToTArray(nsIArray* messages, nsTArray<RefPtr<nsIMsgDBHdr>>& out) {
+  if (!messages) {
+    out.Clear();
+    return;
+  }
   uint32_t count;
   messages->GetLength(&count);
   out.Clear();
