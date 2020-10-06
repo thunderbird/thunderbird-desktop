fd85779b65bee4a51db140e2e170c000ee8443c2
Joaquín Serna <bubuanabelas@cryptolab.net>
2020-10-06 01:06:43 -0300

    Bug 1669044 - Add MailFolder's subfolders to displayedFolder and selectedFolder. r=darktrojan  a=wsmwk
    Test additions by darktrojan.

--- mail/components/extensions/test/browser/browser_ext_menus.js
+++ mail/components/extensions/test/browser/browser_ext_menus.js
@@ -103,6 +103,7 @@ add_task(async function test_folder_pane() {
   );
   is(info.selectedFolder.accountId, gAccount.key);
   is(info.selectedFolder.path, "/Trash");
+  ok(Array.isArray(info.selectedFolder.subFolders));
   ok(!info.displayedFolder);
   ok(!info.selectedMessages);
 
@@ -129,7 +130,9 @@ add_task(async function test_thread_pane() {
   );
   is(info.displayedFolder.accountId, gAccount.key);
   is(info.displayedFolder.path, "/Trash");
-  is(info.selectedMessages.cursor, null);
+  ok(Array.isArray(info.displayedFolder.subFolders));
+  is(info.selectedMessages.id, null);
+  is(info.selectedMessages.messages.length, 1);
   ok(!info.selectedFolder);
 
   await extension.unload();
